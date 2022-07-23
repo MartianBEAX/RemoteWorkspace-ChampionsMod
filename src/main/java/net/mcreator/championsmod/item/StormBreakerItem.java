631b3251509ab24be16102fb6895d6d888f0f6bb
IttsAllison <asier@192.168.1.45>
2022-07-23 15:33:13 +0200

    -Added Stormbreaker model and texture and made it a tool

--- src/main/java/net/mcreator/championsmod/item/StormBreakerItem.java
+++ src/main/java/net/mcreator/championsmod/item/StormBreakerItem.java
@@ -4,32 +4,23 @@ package net.mcreator.championsmod.item;
 import net.minecraftforge.registries.ObjectHolder;
 
 import net.minecraft.world.World;
-import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.Hand;
-import net.minecraft.util.ActionResult;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Item;
 import net.minecraft.inventory.EquipmentSlotType;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.ai.attributes.Attribute;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.Entity;
-import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.block.BlockState;
 
-import net.mcreator.championsmod.procedures.BrokenMjolnirRightclickProcedure;
 import net.mcreator.championsmod.procedures.BrokenMjolnirInInventoryProcedure;
-import net.mcreator.championsmod.procedures.BrokenMjolnirEntityIsHitProcedure;
 import net.mcreator.championsmod.ChampionsModModElements;
 
 import java.util.stream.Stream;
 import java.util.Map;
-import java.util.List;
 import java.util.HashMap;
 import java.util.AbstractMap;
 
@@ -37,54 +28,17 @@ import com.google.common.collect.Multimap;
 import com.google.common.collect.ImmutableMultimap;
 
 @ChampionsModModElements.ModElement.Tag
-public class BrokenMjolnirItem extends ChampionsModModElements.ModElement {
-	@ObjectHolder("champions_mod:broken_mjolnir")
+public class StormBreakerItem extends ChampionsModModElements.ModElement {
+	@ObjectHolder("champions_mod:storm_breaker")
 	public static final Item block = null;
 
-	public BrokenMjolnirItem(ChampionsModModElements instance) {
-		super(instance, 19);
+	public StormBreakerItem(ChampionsModModElements instance) {
+		super(instance, 29);
 	}
 
 	@Override
 	public void initElements() {
 		elements.items.add(() -> new ItemToolCustom() {
-			@Override
-			public void addInformation(ItemStack itemstack, World world, List<ITextComponent> list, ITooltipFlag flag) {
-				super.addInformation(itemstack, world, list, flag);
-				list.add(new StringTextComponent("\"Whosoever holds this hammer"));
-				list.add(new StringTextComponent("if he be worthy"));
-				list.add(new StringTextComponent("shall possess the power of Thor.\" -Odin"));
-			}
-
-			@Override
-			public ActionResult<ItemStack> onItemRightClick(World world, PlayerEntity entity, Hand hand) {
-				ActionResult<ItemStack> retval = super.onItemRightClick(world, entity, hand);
-				ItemStack itemstack = retval.getResult();
-				double x = entity.getPosX();
-				double y = entity.getPosY();
-				double z = entity.getPosZ();
-
-				BrokenMjolnirRightclickProcedure
-						.executeProcedure(Stream.of(new AbstractMap.SimpleEntry<>("world", world), new AbstractMap.SimpleEntry<>("entity", entity))
-								.collect(HashMap::new, (_m, _e) -> _m.put(_e.getKey(), _e.getValue()), Map::putAll));
-				return retval;
-			}
-
-			@Override
-			public boolean hitEntity(ItemStack itemstack, LivingEntity entity, LivingEntity sourceentity) {
-				boolean retval = super.hitEntity(itemstack, entity, sourceentity);
-				double x = entity.getPosX();
-				double y = entity.getPosY();
-				double z = entity.getPosZ();
-				World world = entity.world;
-
-				BrokenMjolnirEntityIsHitProcedure.executeProcedure(Stream
-						.of(new AbstractMap.SimpleEntry<>("world", world), new AbstractMap.SimpleEntry<>("entity", entity),
-								new AbstractMap.SimpleEntry<>("sourceentity", sourceentity))
-						.collect(HashMap::new, (_m, _e) -> _m.put(_e.getKey(), _e.getValue()), Map::putAll));
-				return retval;
-			}
-
 			@Override
 			public void inventoryTick(ItemStack itemstack, World world, Entity entity, int slot, boolean selected) {
 				super.inventoryTick(itemstack, world, entity, slot, selected);
@@ -97,12 +51,12 @@ public class BrokenMjolnirItem extends ChampionsModModElements.ModElement {
 								new AbstractMap.SimpleEntry<>("itemstack", itemstack))
 						.collect(HashMap::new, (_m, _e) -> _m.put(_e.getKey(), _e.getValue()), Map::putAll));
 			}
-		}.setRegistryName("broken_mjolnir"));
+		}.setRegistryName("storm_breaker"));
 	}
 
 	private static class ItemToolCustom extends Item {
 		protected ItemToolCustom() {
-			super(new Item.Properties().group(ItemGroup.COMBAT).maxDamage(0).isImmuneToFire());
+			super(new Item.Properties().group(ItemGroup.COMBAT).maxDamage(0));
 		}
 
 		@Override
@@ -124,7 +78,7 @@ public class BrokenMjolnirItem extends ChampionsModModElements.ModElement {
 
 		@Override
 		public int getItemEnchantability() {
-			return 0;
+			return 6;
 		}
 
 		@Override
@@ -133,9 +87,9 @@ public class BrokenMjolnirItem extends ChampionsModModElements.ModElement {
 				ImmutableMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
 				builder.putAll(super.getAttributeModifiers(equipmentSlot));
 				builder.put(Attributes.ATTACK_DAMAGE,
-						new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Tool modifier", 6f, AttributeModifier.Operation.ADDITION));
+						new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Tool modifier", 7.5f, AttributeModifier.Operation.ADDITION));
 				builder.put(Attributes.ATTACK_SPEED,
-						new AttributeModifier(ATTACK_SPEED_MODIFIER, "Tool modifier", -2.8, AttributeModifier.Operation.ADDITION));
+						new AttributeModifier(ATTACK_SPEED_MODIFIER, "Tool modifier", -3.2999999999999999, AttributeModifier.Operation.ADDITION));
 				return builder.build();
 			}
 			return super.getAttributeModifiers(equipmentSlot);
